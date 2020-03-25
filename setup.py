a5f663176ff973a451ad4e7f8181889ed3f3ca18
aylliote <elliot.hofman@gmail.com>
2020-03-25 12:42:26 +0100

    fix(corruption): remove datafiles from setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os.path as osp
-
 import setuptools
 
 
@@ -10,6 +8,4 @@ setuptools.setup(
     author='Elliot',
     packages=setuptools.find_packages(),
     install_requires=['numpy==1.18.*', 'requests==2.23.*'],
-    entry_points={ 'console_scripts': ['corruption=corruption.bin.corrupt:main'] },
-    data_files=[('data', [osp.join('corruption', 'data', 'THESAURUS.DAT')])]
-)
+    entry_points={ 'console_scripts': ['corruption=corruption.bin.corrupt:main'] })
